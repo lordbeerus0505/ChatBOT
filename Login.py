4e82adb38affe919dc2907045a09470c9b0b879d
Abhiram Natarajan <abhiram.natarajan@gmail.com>
2018-08-05 00:09:33 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -19,7 +19,7 @@ class Login:
 		global db
 		global cursor
 		log=Login()
-		db=MySQLdb.connect("localhost","root","abhiawesome007","Login_DB")
+		db=MySQLdb.connect("localhost","root","","Login_DB")
 		cursor=db.cursor()
 		cursor.execute("SELECT username FROM credentials")
 		uname=cursor.fetchall()
@@ -68,7 +68,7 @@ class Login:
 		global password
 		global db
 		global cursor
-		db=MySQLdb.connect("localhost","root","abhiawesome007","Login_DB")
+		db=MySQLdb.connect("localhost","root","","Login_DB")
 		cursor=db.cursor()
 		cursor.execute("SELECT username FROM credentials")
 		uname=cursor.fetchall()
